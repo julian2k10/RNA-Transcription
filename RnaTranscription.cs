0c4c6764366b680cf0bba457fe4cc49f9b2e3373
Julian Joseph <42915658+julian2k10@users.noreply.github.com>
2019-11-09 15:44:55 -0500

    Added Project Files

---
+++ RnaTranscription.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class RnaTranscription
+{
+    public static string ToRna(string nucleotide)
+    {
+        List<char> RNA = new List<char>();// Store new RNA sequence
+        Dictionary<char, char> key = new Dictionary<char, char>();//Store key for DNA mapping sequence
+        key.Add('G', 'C');
+        key.Add('C', 'G');
+        key.Add('T', 'A');
+        key.Add('A', 'U');
+        
+        foreach (char letter in nucleotide)
+        {
+            char value = Char.ToUpper(letter);
+            List<char> valid_keys = key.Keys.ToList();
+            if (valid_keys.Contains(value))
+            {
+                RNA.Add(key[value]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Character Detected!\n '{0}' is not a valid DNA nucleotide.", value);
+            }
+        }
+        return String.Join("", RNA);
+    }
+}
